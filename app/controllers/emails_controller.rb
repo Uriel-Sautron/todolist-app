b7ada7e1617d6cc880e9584d30a8a736de273ac5
Uriel-Sautron <uriel.sautron@gmail.com>
2020-11-19 12:00:11 +0100

    add button create email

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,4 +2,22 @@ class EmailsController < ApplicationController
   def index
     @emails = Email.all
   end
+
+  def create
+    @email = Email.new(
+      object: Faker::Company.buzzword,
+      body: Faker::Lorem.paragraphs)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js {}
+      if @email.save
+        flash[:notice] = "Email created"
+        
+      else
+        redirect_to root_path
+        flash[:notice] = "Please try again"
+      end
+    end
+  end
+
 end
